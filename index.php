f08fd42c01577dcc5ae569a13ee000189c6e93ac
HassanAyubSiddique <hassanayubsiddique@gmail.com>
2024-03-12 16:26:13 +0000

    allss
    test

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/logo.png">
+    <title>Home Heaven</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+    <header>
+        <!-- Main navigation menu -->
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="Productpagehtml.php">Products</a></li>
+                <li><a href="Customerloginhtml.php">Customer Portal</a></li>
+                <li><a href="Contactushtml.php">Contact Us</a></li>
+            </ul>
+        </nav>
+
+        <!-- Admin navigation menu -->
+        <nav>
+            <ul>
+                <li><a href="Adminportal.php">Admin</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <br>
+
+    <h1><a href="index.php">Home Heaven</a></h1>
+
+    <br>
+
+    <h2>Unlock a World of Endless Possibilities with Our Alluring Product Range</h2>
+
+    <br>
+
+    <div class="slideshow">
+        <!-- Slideshow images -->
+        <img src="images/slide1.jpg" class="active">
+        <img src="images/slide2.jpg">
+        <img src="images/slide3.jpg">
+        <img src="images/slide4.jpg">
+        <img src="images/slide5.jpg">
+    </div>
+
+    <br>
+
+    <div class="center">
+        <a href="Productpagehtml.php" class="button">Shop now!</a>
+    </div>
+
+    <br>
+
+    <footer>
+        <div class="footer-left">
+            <!-- Social media icons -->
+            <a href="https://www.instagram.com/"><img src="images/instagram.png" alt="Instagram"></a>
+            <a href="https://en-gb.facebook.com/"><img src="images/facebook.png" alt="Facebook"></a>
+            <a href="https://uk.linkedin.com/?original_referer=https%3A%2F%2Fwww.google.com%2F"><img src="images/linkedin.png" alt="Linkedin"></a>
+        </div>
+        <div class="footer-right">
+            &copy; 2023 Home Heaven. Allss Rights Reserved.
+        </div>
+    </footer>
+    
+    <script src="java/slideshow.js"></script>
+</body>
+</html>
